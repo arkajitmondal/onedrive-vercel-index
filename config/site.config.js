25fe5068aec0bdbed619edff45785bb4a2ac0de1
Arkajit Mondal <arkajitm2@gmail.com>
2024-08-09 17:34:46 +0530

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -38,7 +38,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    '<a href="https://github.com/arkajitmondal" target="_blank" rel="noopener noreferrer">Onedrive index</a> Made with ❤ by Arkajit.',
+    '<a href="https://github.com/arkajitmondal" target="_blank" rel= Made with ❤ by Arkajit.',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
